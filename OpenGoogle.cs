8e5bd4a916f6fba037f9a674fb2d298b1b0a44ee
Shalabh Varma <varma.shalabh@gmail.com>
2018-05-16 15:00:55 +1000

    Add files via upload

---
+++ OpenGoogle.cs
@@ -0,0 +1,22 @@
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace hello_google_with_webdriver
+{
+    public class OpenGoogle
+    {
+        [Fact]
+        public void openGoogle()
+        {
+            IWebDriver driver = new ChromeDriver("../netcoreapp2.0/");
+
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Close();
+
+            Console.WriteLine("Test complete");
+
+        }
+    }
+}
